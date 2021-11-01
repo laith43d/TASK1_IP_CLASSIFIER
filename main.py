1af0469f7cb37bead326cb2b3d71d91e13d13d72
manhal <ism21_1_79@uoitc.edu.iq>
2021-11-01 17:50:06 +0300

    Manhal_maath

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import sys
+
+
 def solution():
-    pass
+    full_ip = sys.argv[1]
+    ip = full_ip.split("/")[0]
+    ip = ip.split(".")
+    if int(ip[0]) == 127 and 0 <= int(ip[1]) <= 255:
+        print('Class A, Designation: Special')
+    elif 1 <= int(ip[0]) <= 126:
+        if int(ip[0]) == 10:
+            print('Class A, Designation: private')
+        else:
+            print('Class A, Designation: public')
+    elif 128 <= int(ip[0]) <= 191:
+        if ((int(ip[0]) == 172) and (16 <= int(ip[1]) <= 31)) or ((int(ip[0]) == 169) and (int(ip[1]) == 254)):
+            print('Class B, Designation: private')
+        else:
+            print('Class B, Designation: public')
+    elif 192 <= int(ip[0]) <= 223:
+        if int(ip[0]) == 192 and (int(ip[1]) == 168):
+            print('Class C, Designation: private')
+        else:
+            print('Class C, Designation: public')
+    elif 224 <= int(ip[0]) <= 239:
+        print('Class D,Designation: Special')
+    elif 240 <= int(ip[0]) <= 255:
+        print('Class E, Designation: Special')
+    else:
+        print("enter the ip in form of x.x.x.x/x")
 
 
 if __name__ == '__main__':
-    pass
+    solution()
+else:
+    print("enter the ip in form of x.x.x.x/x")
