f5330b67766a3abd41c6f5e202283ccf38c12a62
alialatabi <ali.alatabi99.vb@gmail.com>
2022-03-19 21:13:21 +0300

    converted the string input to a list of integers and splited the mask

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def solution():
-    pass
+    ipa = input('\nPlease give an IP address: ')
+    mask = ipa.split('/')
+    split_ip = mask[0]
+    int_ip = split_ip.split('.')
+
+    for i in range(0, len(int_ip)):
+        int_ip[i] = int(int_ip[i])
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    pass
+    solution()
