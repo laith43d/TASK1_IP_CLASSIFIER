5179f85bbd7da061536ccbd228c164a28fb2ba94
maath <steelsinew@gmail.com>
2022-03-20 14:47:56 +0300

    IP address validation, detection of class and designation

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import helpers
+from classes.ip import IP
+
 def solution():
-    pass
+    value = input('Please give an IP address (IP/MASK): ') 
+
+    try:
+        ip = IP(value)
+    except:
+        return print(helpers.error_message)
+    
+
+    if(helpers.is_valid_ip(ip) == False):
+        return print(helpers.error_message)
+
+
+    print(helpers.get_info(ip))
 
 
 if __name__ == '__main__':
-    pass
+    solution()
