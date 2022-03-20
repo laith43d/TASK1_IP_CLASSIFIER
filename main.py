19305a39f87864a6faf89a3eda8de8c006b4a361
Rasheed <rasheedwathiq11@gmail.com>
2022-03-20 05:44:56 +0300

    the solution of task-

--- main.py
+++ main.py
@@ -14,28 +14,30 @@ def solution():
 
     
 
-    if ipA[0]>=1 and ipA[0]<=127:
+    if ipA[0]>=1 and ipA[0]<=126:
         # print("aaaa")
         if(ipA[0]==10):
             print("Class: A, Designation: Private")
         else:
-            print("Class: A, Designation: Special")
+            print("Class: A, Designation: Public")
+    elif ipA[0]==127:
+        print("Class: A, Designation: Special")
     elif ipA[0]>=128 and ipA[0]<=191:
         if ipA[0]== 172 and ipA[1]>=16 and ipA[1]<=31:
             print("Class: B, Designation: Private")
         else:
-            print("Class: B, Designation: Special")
+            print("Class: B, Designation: Public")
 
     elif ipA[0]>=192 and ipA[0]<=223:
         if ipA[0]== 192 and ipA[1]>=168:
             print("Class: C, Designation: Private")
         else:
-            print("Class: C, Designation: Special")
+            print("Class: C, Designation: Public")
 
     elif ipA[0]>=224 and ipA[0]<=239:
-        print("Class: D, Designation: Special")
+        print("Class: D, Designation: Public")
     elif ipA[0]>=240 and ipA[0]<=255:
-        print("Class: E, Designation: Special")
+        print("Class: E, Designation: Public")
 
 if __name__ == '__main__':
     solution()
