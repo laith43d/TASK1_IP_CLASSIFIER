ca3069ba295b52732599efc0bf35eefc707cc511
Rasheed <rasheedwathiq11@gmail.com>
2022-03-20 05:16:26 +0300

    the solution of task

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 def solution():
-    pass
+    a=input("Enter the IP Address:")
+    b=a.split("/")
+    ipA= b[0].split('.')
+    ipA=[int(i) for i in ipA]
+    # print(ipA)
 
+    for j in ipA:
+        if j<0 or j>255:
+            print("invalid ip address")
+            break
+
+
+
+    
+
+    if ipA[0]>=1 and ipA[0]<=127:
+        # print("aaaa")
+        if(ipA[0]==10):
+            print("Class: A, Designation: Private")
+        else:
+            print("Class: A, Designation: Special")
+    elif ipA[0]>=128 and ipA[0]<=191:
+        if ipA[0]== 172 and ipA[1]>=16 and ipA[1]<=31:
+            print("Class: B, Designation: Private")
+        else:
+            print("Class: B, Designation: Special")
+
+    elif ipA[0]>=192 and ipA[0]<=223:
+        if ipA[0]== 192 and ipA[1]>=168:
+            print("Class: C, Designation: Private")
+        else:
+            print("Class: C, Designation: Special")
+
+    elif ipA[0]>=224 and ipA[0]<=239:
+        print("Class: D, Designation: Special")
+    elif ipA[0]>=240 and ipA[0]<=255:
+        print("Class: E, Designation: Special")
 
 if __name__ == '__main__':
-    pass
+    solution()
