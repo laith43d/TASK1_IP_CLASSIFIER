1811ebc36dc7dee3a65ed033da90a9ee907110a7
unknown <moh8411@gmail.com>
2022-03-22 02:11:24 +0300

    upload solution

---
+++ ip.py
@@ -0,0 +1,99 @@
+def checkIP(testIpClass):
+        """
+        This method calculates the class of the given IP address.
+        Returns a string 'A', 'B', 'C', 'D' or 'E'
+        """    
+        workingIP = [0, 0, 0, 0]
+        ipmask = testIpClass.split('/')
+        splitIP = ipmask[0].split('.')
+
+        for index in range(len(splitIP)):
+            workingIP[index] = int(splitIP[index])
+            if workingIP[index] > 255: raise ValueError
+            if index > 3: raise ValueError
+        return workingIP , ipmask[0]
+
+
+def getClass(workingIP):
+        """
+        This method calculates the class of the given IP address.
+        Returns a string 'A', 'B', 'C', 'D' or 'E'
+        """
+
+        ipClass = ''
+        test = workingIP[0]
+  
+        if test >= 0 and test < 127:
+            ipClass = 'A'
+
+        if test == 127:
+            if workingIP[1]+workingIP[2]+workingIP[3] == 0:
+                ipClass = 'A'
+            else: 
+                ipClass = 'C'
+
+        elif test > 127 and test <= 191:
+            ipClass = 'B'
+
+        elif test > 191 and test <= 223:
+            ipClass =  'C'
+
+        elif test > 223 and test <= 239:
+            ipClass = 'D'
+
+        elif test > 239 and test <= 255:
+            ipClass = 'E'
+
+        return ipClass
+
+
+
+def getDesignation(workingIP):
+        """
+        This method Return the designation ipDesignation as a string
+        """
+
+        ipDesignation = ''
+        ipClass = getClass(workingIP)
+#class A
+        if ipClass == 'A':
+            if workingIP[0] == 10:
+                ipDesignation = 'Internet Private Address'
+
+            elif workingIP[0] == 127:
+                ipDesignation = 'Local Host Address'
+            else:
+                ipDesignation = 'Internet Public Address'
+#class B
+        elif ipClass == 'B':
+            if workingIP[0] == 172 and workingIP[1] >= 16 and workingIP[1] <= 31:
+                ipDesignation = 'Internet Private Address'
+            else:
+                ipDesignation = 'Internet Public Address'            
+
+#class C
+        elif ipClass == 'C':
+            if workingIP[0] == 192 and workingIP[1] == 168:
+                ipDesignation =  'Internet Private Address'
+
+            elif workingIP[0] == 127:
+                ipDesignation = 'Internet Special IP Addresses'
+                
+            else:
+                ipDesignation = 'Internet Public Address'
+
+#class D
+        elif ipClass == 'D':
+ 
+            ipDesignation = 'Multi-Cast'
+
+#class E
+        elif ipClass == 'E':
+
+            ipDesignation = Research/Reserved/Experimental
+
+        else:
+            ipDesignation = 'Internet Public Address'
+
+        return ipDesignation
+
--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import ip
+
 def solution():
     pass
 
 
+
+
+
+
+
+givenIP = input('\nPlease give an IP address (IP/MASK): ')
+try:
+    myWorkingIP=ip.checkIP(givenIP)
+    print('Your IP     : ' +myWorkingIP[1])
+    print('Class       : ' + ip.getClass(myWorkingIP[0]))
+    print('Designation : ' + ip.getDesignation(myWorkingIP[0]))
+except (IndexError, ValueError):
+    print('\nPlease give a valid IPv4/MASK address in #.#.#.#/# format,' '\n'
+    '\tfor example: 192.168.1.1/24, or 10.10.10.1/28')
+    main()
+
 if __name__ == '__main__':
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
