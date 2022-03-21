55dabdc9a7d75298695eed007c7c5bee98d620d1
Zainab Salim <zainab_aldahlaky.com>
2022-03-22 01:50:23 +0300

    solve ip calculater task

---
+++ ip_rang_clac.py
@@ -0,0 +1,77 @@
+
+
+from tokenize import String
+from xmlrpc.client import Boolean
+
+
+def find_ip_range(ip_add):
+    if ip_add >=1 and (ip_add) <=127:
+
+        return 'A'
+
+    elif (ip_add) >=128 and (ip_add) <=191:
+        return 'B'
+
+    elif (ip_add) >=192 and (ip_add) <=223:
+        return 'C'
+
+    # elif (ip_add) >= 224 and  (ip_add) <=239:
+    #     return 'D'
+
+    # elif (ip_add) >=240 and  (ip_add)<=255:
+    #     return'E'
+
+    else:
+        return '1'
+
+
+def find_desginateA(ip_address : list) -> String:
+    if ip_address[0]==10:
+        return 'private'
+    elif ip_address[0]==172 and (ip_address[1] >=16 and ip_address[1] <=31) :
+        return 'private'
+    elif ip_address[0]==169 and (ip_address[1] ==254) :
+        return 'private'
+    elif ip_address[0]==192 and (ip_address[1] ==168) :
+        return 'private'
+    else:
+        return 'public'
+
+
+# def find_desginateB (ip_address : list) -> String:
+#     if ip_address[0]==172 and (ip_address[1] >=16 and ip_address[1] <=31) :
+#         return 'private'
+#     elif ip_address[0]==169 and (ip_address[1] ==254) :
+#         return 'private'
+#     else:
+#         return 'public'
+
+
+
+# def find_desginateC (ip_address : list) -> String:
+#     if ip_address[0]==192 and (ip_address[1] ==168) :
+#         return 'private'
+
+#     else:
+#         return 'public'
+
+
+
+
+def check_if_special(ip_address : list) -> Boolean:
+    if ip_address[0]==127:
+        if ip_address[1]==0 and ip_address[2] ==0 and  ip_address[3]==0:
+            return 'Class: A, Designation: Public'
+        else:
+            return 'Class: C, Designation: Special'
+
+    elif ip_address[0] >= 224 and  ip_address[0] <=239:
+        return 'Class: D, Designation: Special'
+
+    elif ip_address[0]>=240 and   ip_address[0] <=255:
+        return 'Class: E, Designation: Special'
+
+    else:
+        return '0'
+
+   
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import ip_rang_clac
+
+
 def solution():
-    pass
+    my_ip=input('Enter Ip Address in the following format : x.x.x.x/x')
+    print (my_ip)
+
+    ip_address=my_ip[0:my_ip.index('/')]
+    list_ip_address=ip_address.split('.')
+    new_int_iplist=[int (i) for i in list_ip_address ]
+   
+# first, check if the IP is special
+
+    res=ip_rang_clac.check_if_special(new_int_iplist) 
+    if res !='0':
+        print(res)
+    else:
+        first_oct=new_int_iplist[0]
+        rang=ip_rang_clac.find_ip_range(int(first_oct))
+        print ('Class : ',rang,', Designation : ',ip_rang_clac.find_desginateA(new_int_iplist))
 
 
 if __name__ == '__main__':
-    pass
+    solution()
