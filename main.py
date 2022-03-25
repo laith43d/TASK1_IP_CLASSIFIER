0e1d96db297106adc11e7d4a0bce05cf7cda70fd
IbrahimMahdi97 <ttrkan35@gmaail.com>
2022-03-26 01:45:35 +0300

    chore: figuring the solution for the ip classifier

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 def solution():
-    pass
+    full_ip = input('enter the full ip in the form x.x.x.x/x: ')
+    ip = full_ip.split('/')[0]
+    subnet = full_ip.split('/')[1]
+
+    octets = ip.split('.')
+    first_octet = int(octets[0])
+    second_octet = int(octets[1])
+    third_octet = int(octets[2])
+    fourth_octet = int(octets[3])
+
+    if (first_octet == 10 and 0 <= second_octet <= 255 and 0 <= third_octet <= 255 and 0 <= fourth_octet <= 255) or (first_octet == 169 and second_octet == 254 and 0 <= third_octet <= 255 and 0 <= fourth_octet <= 255) or (first_octet == 172 and 16 <= second_octet <= 31 and 0 <= third_octet <= 255 and 0 <= fourth_octet <= 255) or (first_octet == 192 and second_octet == 168 and 0 <= third_octet <= 255 and 0 <= fourth_octet <= 255):
+        print('Private Network')
+    elif first_octet == 127 and 0 <= second_octet <= 255 and 0 <= third_octet <= 255 and 1 <= fourth_octet <= 255:
+        print('Special Network')
+    else:
+        print('Public Network')
 
 
 if __name__ == '__main__':
-    pass
+    solution()
