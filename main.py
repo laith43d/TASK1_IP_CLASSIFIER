4a94b45bf63c628fb1753a1ce1bf6ad14d90afa9
HaiderAlalawi <figogood89@gmail.com>
2022-07-10 15:32:08 +0300

    HAIDER ABDULAMEER TASK1 SOLUTION

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def solution():
-    pass
+def solution(ip):
+    first, second, third, fourth = map(int, ip.split('.'))
+
+    if first > 255 or second > 255 or third > 255 or fourth > 255:
+        print("INVALID IP ADDRESS")
+        exit()
+
+    if 0 < first <= 127:
+        classtype = 'A'
+        designation = "Public"
+        if first == 10:
+            designation = 'Private'
+
+        if first == 127:
+            designation = "Special"
+
+    if 128 <= first <= 191:
+        classtype = 'B'
+        designation = "Public"
+        if first == 172 and 16 <= second <= 31:
+            designation = 'Private'
+
+    if 192 <= first <= 223:
+        classtype = 'C'
+        designation = "Public"
+        if first == 192 and second == 168:
+            designation = 'Private'
+
+    if 224 <= first <= 239:
+        classtype = 'D'
+        designation = "Public"
+
+    if 240 <= first <= 255:
+        classtype = 'E'
+        designation = "Public"
+
+    print("Class: {}, Designation: {}".format(classtype, designation))
 
 
 if __name__ == '__main__':
-    pass
+    solution(input("Enter IP ADDRESS: "))
+
