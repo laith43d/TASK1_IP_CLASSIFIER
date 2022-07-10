8aed6300fb8860f3d286d952ca8cdbcd6e7dc708
abd2oo2 <cs.20.156@student.uotechnology.edu.iq>
2022-07-10 19:43:58 +0300

    some modifications

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 def solution(ip):
     ip2 = ip
     
-    x = ip3 = 0
+    x = d = ip3 = 0
     for i in  range(len(ip)): 
         if ip[i] == '.':
             x = i
-            # ip3 = int (ip2[x +1 : x+ 4])
             ip =int( ip[0:x])
             #class A
             if ip > 0  and ip < 128 :
+                for i in range(len(ip2)):
+                    if ip2[i] == "/":
+                        d = int(ip2[i -1 ])
                 print("IP address class (A)")
+                
                 if ip == 10 : 
                     print("IP address designation (Private)")
+                elif ip == 127 and d >= 1 :
+                    print("ip address designation (special)")
                 else :
                     print("IP address designation (Public)")
             elif ip >= 128 and ip < 191 : 
@@ -25,7 +30,7 @@ def solution(ip):
                 #class c
                 print("IP address class (C)")
                 h = len(ip2)
-                if h == 11 :
+                if h == 11 or h== 14 :
                     ip3 = int (ip2[x +1 : x+ 4])
                 if ip3  == 168: 
                     print("IP address designation (Private)")
