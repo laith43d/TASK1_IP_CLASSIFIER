9ff2ac753d490c9d8edb65965d8963db4fba3d60
Saif Alhaider <saifharith90@gmail.com>
2022-07-10 17:38:13 +0300

    changed variable's names due to confusion

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ def solution(ip):
     # ip validation
     if len(ip) != 4 :print("ip is not correct");exit()
     try:
-        A, B, C, D = int(ip[0]), int(ip[1]), int(ip[2]),int(ip[3])
+        part1, part2, part3, part4 = int(ip[0]), int(ip[1]), int(ip[2]),int(ip[3])
     except ValueError:
         print("ip is not correct")
         exit()
-    if ((A or B or C or D) not in range(0,256)):
+    if ((part1 or part2 or part3 or part4) not in range(0,256)):
         print("Please check if the ip is in range")
         exit()
     # boolean values
-    publicA = A in range(0,128)
-    publicB = A in range(128,192)
-    publicC = A in range(192,224)
-    publicD = A in range(224,240)
+    publicA = part1 in range(0,128)
+    publicB = part1 in range(128,192)
+    publicC = part1 in range(192,224)
+    publicD = part1 in range(224,240)
 
-    privateA = A == 10
-    privateB = A == 172 and B in range(16,32)
-    privateC = A == 192 and B == 168
+    privateA = part1 == 10
+    privateB = part1 == 172 and part2 in range(16,32)
+    privateC = part1 == 192 and part2 == 168
 
-    special = A == 127 and D != 0
+    special = part1 == 127 and part4 != 0
 
     # check type
     if special:print("class A special")
