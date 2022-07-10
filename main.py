c754120413fe661fe805ef58e0a6e173283beb2a
Fatima Salah <alanyfsalah@gmail.com>
2022-07-10 21:53:57 +0300

    Editing NO.2 by Fatima Salah

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-def solution():
-    IP=input("Enter IP address,writ it in this style IP/MASK:")
+def solution(): #Function
+    #Enter IP From User
+    IP=input("Enter IP address,writ it in this style IP/MASK:") 
     print(IP)
-    if IP>="1.0.0.0" and IP<="127.0.0.0":
+    #Class Ranges and Designation Types
+    if IP>="1.0.0.0" and IP<="127.0.0.0":#For Class A (Public)
      IP_Class="A"
      IP_Type="Public"
-     if IP>="10.0.0.0" and IP<="10.255.255.255":
+     if IP>="10.0.0.0" and IP<="10.255.255.255":#For Class A (Private)
         IP_Class="A"
         IP_Type="Private"
-    elif IP>="128.0.0.0" and IP<="191.255.0.0":
+    elif IP>="128.0.0.0" and IP<="191.255.0.0":#For Class B (Public)
      IP_Class="B"
      IP_Type="Public"
-     if IP>="172.16.0.0" and IP<="172.31.255.255":
+     if IP>="172.16.0.0" and IP<="172.31.255.255":#For Class B (Private)
         IP_Class="B"
         IP_Type="Private"
-    elif IP>="192.0.0.0" and  IP<="223.255.255.0":
+    elif IP>="192.0.0.0" and  IP<="223.255.255.0":#For Class C (Public)
      IP_Class="C"
      IP_Type="Public"
-    elif IP>="192.168.0.0" and IP<="192.168.255.255":
+    elif IP>="192.168.0.0" and IP<="192.168.255.255":#For Class C (Private)
         IP_Class="B"
         IP_Type="Private"
-    elif IP>="127.0.0.1" and IP<="127.255.255.255":
+    elif IP>="127.0.0.1" and IP<="127.255.255.255":#For Class C (Special)
         IP_Class="B"
         IP_Type="Special"
-    elif IP>="224.0.0.0" and IP<="239.255.255.255":
+    elif IP>="224.0.0.0" and IP<="239.255.255.255":#For Class D (Public)
      IP_Class="D"
      IP_Type="Public"
-    elif IP>="240.0.0.0" and IP<="255.255.255.255":
+    elif IP>="240.0.0.0" and IP<="255.255.255.255":#For Class E (Public)
      IP_Class="E"
      IP_Type="Public"
-    print("Class: "+IP_Class +", Designation: " +IP_Type)
+    print("Class: "+IP_Class +", Designation:" +IP_Type) #Print OutPut,The Class's type and Designation's type that the user has entered
 
 if __name__ == '__main__':
-     solution()
+     solution() #Call The Function
