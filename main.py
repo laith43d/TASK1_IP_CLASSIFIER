8b195b828537b09e91c51c7c5fe9311500ad1214
teb <85996160+Teebak@users.noreply.github.com>
2022-07-11 19:00:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 def solution():
-    pass
 
+
+    IP = input("write your IP address : ip/mask ")
+
+#split the ip address & mask
+    ip= IP.split('/')
+
+
+    ip_split = ip[0].split('.')
+
+    octet1 = (int(ip_split[0]))
+    octet2 = (int(ip_split[1]))
+    octet3 = (int(ip_split[2]))
+    octet4 = (int(ip_split[3]))
+
+
+#class Types
+
+    if (octet1) in range(0, 128):
+     print("class Type = A")
+    elif (octet1) in range(128, 192):
+     print("class Type = B")
+    elif (octet1) in range(192, 224):
+     print("class Type = C")
+    elif (octet1) in range(224, 240):
+     print("class Type = D")
+    elif (octet1) in range(240, 256):
+     print("class Type = E")
+
+#Designation Types
+
+    if octet1 == 10:
+         print(" IP Designation is : Private")
+    elif octet1 == 172 and octet2 >= 16 and octet2 <= 31:
+         print(" IP Designation is : Private ")
+    elif octet1 == 192 and octet2 == 168:
+         print(" IP Designation is : Private ")
+    elif octet1 == 127 and octet4 in range (1,255):
+         print(" IP Designation is : Special ")
+    else:
+         print(" IP Address is : Public")
 
 if __name__ == '__main__':
-    pass
+
+    solution()
+
+# & I quote "more is less" <3
