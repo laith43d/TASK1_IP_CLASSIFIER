bce313bc6d9262452fd9bbb375a42312b0148386
Fatima Salah <alanyfsalah@gmail.com>
2022-07-13 01:20:52 +0300

    git commit main.py

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
+
 def solution(): #Function
-    #Enter IP From User
-    IP=input("Enter IP address,writ it in this style IP/MASK:") 
-    print(IP)
-    #Class Ranges and Designation Types
-    if IP>="1.0.0.0" and IP<="127.0.0.0":#For Class A (Public)
-     IP_Class="A"
-     IP_Type="Public"
-     if IP>="10.0.0.0" and IP<="10.255.255.255":#For Class A (Private)
+
+    #For Public IP Address
+    if IP_Address[0]>=1 and IP_Address[0]<=9 or IP_Address[0]>=11 and IP_Address[0]<=126 :
+        IP_Class="A"
+        IP_Type="public"
+    elif IP_Address[0]>=128 and IP_Address[0]<=191:
+        IP_Class="B"
+        IP_Type="public"
+    elif IP_Address[0]>=192 and IP_Address[1]< 168 and IP_Address[0]<=223:
+        IP_Class="C"
+        IP_Type="public"
+
+    #For Private IP Address
+    if IP_Address[0]==10 :
         IP_Class="A"
         IP_Type="Private"
-    elif IP>="128.0.0.0" and IP<="191.255.0.0":#For Class B (Public)
-     IP_Class="B"
-     IP_Type="Public"
-     if IP>="172.16.0.0" and IP<="172.31.255.255":#For Class B (Private)
+    elif IP_Address[0]==169 and IP_Address[0]==254:
+        IP_Class="B"
+        IP_Type="Private APIPA"
+    elif IP_Address[0]==172 and 16<= IP_Address[1] <=31 :
         IP_Class="B"
         IP_Type="Private"
-    elif IP>="192.0.0.0" and  IP<="223.255.255.0":#For Class C (Public)
-     IP_Class="C"
-     IP_Type="Public"
-    elif IP>="192.168.0.0" and IP<="192.168.255.255":#For Class C (Private)
-        IP_Class="B"
+    elif IP_Address[0]==192 and IP_Address[1]==168:
+        IP_Class="C"
         IP_Type="Private"
-    elif IP>="127.0.0.1" and IP<="127.255.255.255":#For Class C (Special)
-        IP_Class="B"
+
+    #For Special IP Address
+    if IP_Address[0]==127:
+        IP_Class="A"
         IP_Type="Special"
-    elif IP>="224.0.0.0" and IP<="239.255.255.255":#For Class D (Public)
-     IP_Class="D"
-     IP_Type="Public"
-    elif IP>="240.0.0.0" and IP<="255.255.255.255":#For Class E (Public)
-     IP_Class="E"
-     IP_Type="Public"
-    print("Class: "+IP_Class +", Designation:" +IP_Type) #Print OutPut,The Class's type and Designation's type that the user has entered
+    elif IP_Address[0]>=224 and IP_Address[0]<=239:
+        IP_Class="D"
+        IP_Type="Special"
+    elif IP_Address[0]>=240 and IP_Address[0]<=255:
+        IP_Class="E"
+        IP_Type="Special"
+
+    
+    print("Class: "+IP_Class ,"Descintion: "+IP_Type)
 
 if __name__ == '__main__':
-     solution() #Call The Function
+    IP=input("Enter Your IP Address, In This Way IP/MASK: ")
+    IP_Mask=IP.split('/')
+    IP_Address=IP_Mask[0].split('.')
+
+    try:
+        IP_Address=[int(i) for i in IP_Address]
+    except ValueError as e:
+        print(e)
+
+    if IP_Address[0]>=1 and IP_Address[0]<=256:
+            solution()
+    else:
+            print("OUT OF THE RANGE....")
