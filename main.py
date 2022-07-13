f48ce19e868eead084d06e036e7408e4055b719b
Zahar Jabar <zzooz162001@gmail.com>
2022-07-13 22:38:56 +0300

    Solution using argv

--- main.py
+++ main.py
@@ -1,57 +1,54 @@
-#user input
-ip = input("Enter IP/Mask : ")
-
-#split the ip address and mask
-ip= ip.split('/')
-mask = ip[1]
-ip_address = ip[0].split('.')
+import sys
+ip = (sys.argv[1]).split('.')
 
 # function to get class type and designation
-def solution(ip_address):
+def solution(part1 , part2): 
+
     #class type
-    if int(ip_address[0]) in range(0, 128):
+    if int(part1) in range(0, 128):
         class_Type = "A"
-    elif int(ip_address[0]) in range(128, 192):
+    elif int(part1) in range(128, 192):
         class_Type = "B"
-    elif int(ip_address[0]) in range(192, 224):
+    elif int(part1) in range(192, 224):
         class_Type = "C"
-    elif int(ip_address[0]) in range(224, 240):
+    elif int(part1) in range(224, 240):
         class_Type = "D"
-    elif int(ip_address[0]) in range(240, 256):
+    elif int(part1) in range(240, 256):
         class_Type = "E"
-    
 
     #designation
-    if (int(ip_address[0]) == 10 ) or (int(ip_address[0]) == 172 and int(ip_address[1]) in range(16, 32)) or (int(ip_address[0]) == 192 and int(ip_address[1]) == 168) :
+    if (int(part1) == 10 ) or (int(part1) == 172 and int(part2) in range(16, 32)) or (int(part1) == 192 and int(part2) == 168) :
         designation = "Praivate"
-    elif int(ip_address[0]) == 127 and int(ip_address[3]) in range(1, 256):
+    elif int(part1) == 127 :
         designation = "Special"
     else: 
         designation = "Public"
 
     return class_Type , designation
-
-
-
+    
 if __name__ == '__main__':
 
-    # assign the returns values to variables
-    class_type, designation = solution(ip_address)
+#input validation
+    try:
+        ip_oc1 = int(ip[0])
+        ip_oc2 = int(ip[1])
+        ip_oc3 = int(ip[2])
+        ip_oc4 = int(ip[3])
 
+        #check ip range
+        if len(ip) == 4 and (ip_oc1 in range(0, 256)) and (ip_oc2 in range(0, 256)) and (ip_oc3 in range(0, 256)) and (ip_oc4 in range(0, 256)):
+            check_ip_range = True    
+        
+            #assign the return values to variables
+            class_type, designation = solution(ip_oc1, ip_oc2)
 
-    #check the validation of the ip
-    i=0
-    while i < len(ip_address):
-        if int(ip_address[i]) in range(0, 256) :
-            check_ip_range = True
+            #print the output
+            if check_ip_range == True:
+                print("Class: " + class_type +", "+ "Designation: " + designation)
+            else:
+                print("Unvalid IP address")
         else:
-            check_ip_range = False
-        i+=1
-    
-    
-    #print the output
-    if check_ip_range == True:
-        print("Class: " + class_type +", "+ "Designation: " + designation)
-    else:
-        print("Unvalid IP address")
+            print("Unvalid IP address")
 
+    except ValueError:
+        print("Unvalid IP address")
