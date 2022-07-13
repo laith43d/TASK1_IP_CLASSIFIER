5f0d490cc5a08947b88754b2fc57837df4aaae29
murtada666 <91660848+murtada666@users.noreply.github.com>
2022-07-13 16:22:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 #murtada hayder
-
+#If you don't like my code you should check your eyes.
 import re
 
-def solution(value):
+def solution(ip):
+
+    value = ip.split(".")
+    first_value = int(value[0])
     #A
-    if (value>str(0) and value<str(128)):
-        if (re.match(r"^10\.", value)):
+    if (first_value > 0 and first_value < 128):
+        if (re.match(r"^10\.", ip)):
             return "Output: Class: A, Designation: Private"
 
-        elif (re.match(r"^127\.1/\.|^127\.", value)):
+        elif (re.match(r"^127\.1/\.|^127\.", ip)):
             return "Output: Class: A, Designation: Special"
 
         else:
             return "Output: Class: A, Designation: Public"
                 
     #B
-    elif (value>str(127) and value<str(192)):
-        if (re.match(r"(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", value)):
+    elif (first_value > 127 and first_value < 192):
+        if (re.match(r"(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", ip)):
             return "Output: Class: B, Designation: Private"
-        elif (re.match(r"^169\.254\.", value)):
+        elif (re.match(r"^169\.254\.", ip)):
             return "Output: Class: B, Designation: Private"
 
         else:
             return "Output: Class: B, Designation: Public"
     #C
-    elif (value>str(191) and value<str(224)):
-        if (re.match(r"^192\.168\.", value)):
+    elif (first_value > 191 and first_value < 224):
+        if (re.match(r"^192\.168\.", ip)):
             return "Output: Class: C, Designation: Private"
 
         else:
             return "Output: Class: C, Designation: Public"
                 
     #D
-    elif (value>str(223) and value<str(240)):
+    elif (first_value> 223 and first_value < 240):
         return "Output: Class: D, Designation: Public"
 
     #E
-    elif (value>str(239) and value<str(256)):
+    elif (first_value > 239 and first_value < 256):
         return "Output: Class: E, Designation: Public"
 
     else:
@@ -49,4 +52,3 @@ if __name__ == '__main__':
     
     print(solution("127.0.0.1/24"))
     print(solution("192.168.1.1/24"))
-
