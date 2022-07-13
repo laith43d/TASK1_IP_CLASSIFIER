9a0220ae9b3ff634019dd5549e021fafc3a221aa
Saif Alhaider <saifharith90@gmail.com>
2022-07-13 16:26:36 +0300

    function recursion

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-def solution(ip):
+def solution():
+    print("Please Enter an Ip adress: " , end='')
+    ip = input()
     ip = ip.split("/")[0].split(".")
     # ip validation
-    if len(ip) != 4 :print("ip is not correct");exit()
+    if len(ip) != 4 :print("ip is not correct\n");solution()
     try:
         part1, part2, part3, part4 = int(ip[0]), int(ip[1]), int(ip[2]),int(ip[3])
     except ValueError:
-        print("ip is not correct")
-        exit()
+        print("ip is not correct\n")
+        solution()
     if ((part1 or part2 or part3 or part4) not in range(0,256)):
-        print("Please check if the ip is in range")
-        exit()
+        print("Please check if the ip is in range\n")
+        solution()
     # boolean values
     publicA = part1 in range(0,128)
     publicB = part1 in range(128,192)
@@ -33,5 +35,12 @@ def solution(ip):
     elif publicD:print("Class: D, Designation: Public")
     else:print("Class: E, Designation: Public")
     
+    print("do you want to try again [Y/N]: ".capitalize())
+    x = input()
+    if x == "y" or x == "Y":
+        solution()
+    else:
+        exit()
+    
 if __name__ == '__main__':
-    solution(input())
+    solution()
