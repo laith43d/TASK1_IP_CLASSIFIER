4e9f2601f597e61bee9e25c91c184ec86d8aa9a3
Ahmed Amer Abd AL-Azeez <80992251+Droxt1@users.noreply.github.com>
2022-07-13 10:38:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,62 @@
-ip = str(input("Please enter the ip address: "))
-numslice = ip.split('.')
+while True:
 
-#Octet 1,2 slicer
-q1 = int(numslice[0])
-q2 = int(numslice[1])
+    try:
 
-#Octet 4 slicer 
-q4slice = numslice[3].split('/')
-q4slice2 = q4slice[0].split('.')
-q4=int(q4slice2[0])
+        IP = str(input("Please enter the IP address: "))
+        numslice = IP.split('.')
 
-#Binary slicing 
-SubnetMask = ip.split('/')
-SubnetMask_Index = SubnetMask[1]
-Octet = ('.'.join([bin(int(x)+256)[3:] for x in SubnetMask[0].split('.')]))
+        #Octet 1,2 slicer
+        q1 = int(numslice[0])
+        q2 = int(numslice[1])
+    
+        #Binary slicing 
+        SubnetMask = IP.split('/')
+        SubnetMask_Index = SubnetMask[1]
+        Octet = ('.'.join([bin(int(x)+256)[3:] for x in SubnetMask[0].split('.')]))
+    
+        print("............................")
+        print("IP Address:",IP)
+        print("Binary:",Octet)
+        print("Subnet Mask:",SubnetMask_Index)
+        
 
-print("IP Address:",ip)
-print("Binary:",Octet)
-print("Subnet Mask:",SubnetMask_Index)
+        def Class_And_IP_Type(): 
+            #Identifying The Class Type  
+            
+            if Octet[0] == '0': 
+                print("Class A") 
 
-def Class_And_IP_Type(): 
-    #Identifying the class type  
-    Result = '' 
-    if Octet[0] == '0': 
-        Result = 'A' 
+            elif Octet[:2] == '10': 
+                print("Class B") 
 
-    elif Octet[:2] == '10': 
-        Result = 'B' 
+            elif Octet[:3] == '110': 
+                print("Class C") 
 
-    elif Octet[:3] == '110': 
-        Result =  'C' 
+            elif Octet[:4] == '1110': 
+                print("Class D") 
 
-    elif Octet[:4] == '1110': 
-        Result = 'D' 
+            elif Octet[:4] == '1111': 
+                print("Class E") 
 
-    elif Octet[:4] == '1111': 
-        Result = 'E' 
+            #Determining The IP Type
 
-    #Determining the IP type
-
-    if q1 == 10:
-        print("Private IP Address")
-    elif q1 == 172 and q2 >= 16 and q2 <= 31:
-        print("Private IP Address")
-    elif q1 == 192 and q2 == 168:
-        print("Private IP Address")
-    elif q1 == 127 and q4 >=1 and q4<=255:
-        print("Special IP Adress")
-    else:
-        print("Public IP Address")
+            if q1 == 10:
+                print("Private IP Address")
+            elif q1 == 172 and q2 >= 16 and q2 <= 31:
+                print("Private IP Address")
+            elif q1 == 192 and q2 == 168:
+                print("Private IP Address")
+            elif q1 == 127:
+                print("Special IP Adress")
+            else:
+                print("Public IP Address")
+   
 
 
-    return(Result)    
-
-
-if __name__ == '__main__':  
-    print("Class: "+Class_And_IP_Type())
+        if __name__ == '__main__':  
+            Class_And_IP_Type()
+            
+        exit()
+    except (IndexError,ValueError,TypeError):
+        print("Please Enter A Valid Input")
+    
