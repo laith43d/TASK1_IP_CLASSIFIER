7c5ab9769d32c5236a1d00cdb1f3e8332eac2e80
Haithem_Ali <eng.haithem0@gmail.com>
2022-07-14 12:27:46 +0300

    new changes

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-def solution():
-    ip = '192.168.1.1/24'
+def solution(ip):
     ip_splited = ip.split('/')
     ip_octets = ip_splited[0].split('.')
-    print(ip_octets)
-    ip_octets = [int(i) for i in ip_splited]
+    ip_octets = [int(i) for i in ip_octets]
+    octet1 = ip_octets[0]
+    octet2 = ip_octets[1]
+    if octet1 == 10:
+        print("Class A, Designation: Private")
+    elif 127 > octet1 >= 1:
+        print("Class A, Designation: Public")
+    elif octet1 == 127:
+        print("Class A, Designation: Special")
+    
+    elif octet1 == 172 and 31 >= octet2 >= 16:
+        print("Class B, Designation: Private ")
+    elif 191 >= octet1 >= 128:
+        print("Class B, Designation: Public")
+    
+    elif octet1 == 192 and octet2 == 168:
+        print("Class C, Designation: Private ")
+    elif 223 >= octet1 >= 192:
+        print("Class C, Designation: Public")
+    
+    elif 239 >= octet1 >= 224:
+        print("Class D, Designation: special")
+    
+    elif  255 >= octet1 >= 240:
+        print("Class D, Designation: special")
     pass
 
 if __name__ == '__main__':
-    solution()
+    ip = '192.168.1.1/24'
+    print('IP =' + ip +' is: ')
+    solution(ip)
     pass 
