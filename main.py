1629fbd468c534b6f57d640f24bcff47794ff9ef
Haithem_Ali <eng.haithem0@gmail.com>
2022-07-14 11:34:11 +0300

    new

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def solution():
+    ip = '192.168.1.1/24'
+    ip_splited = ip.split('/')
+    ip_octets = ip_splited[0].split('.')
+    print(ip_octets)
+    ip_octets = [int(i) for i in ip_splited]
     pass
-#testing
 
 if __name__ == '__main__':
-    pass
+    solution()
+    pass 
