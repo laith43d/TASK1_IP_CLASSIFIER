799f195240987ac92ed1d7cbe5cf6fb5d6559530
Mohammed Qasim <97037415+shukri17@users.noreply.github.com>
2022-07-14 05:23:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,30 @@ def solution(ip):
     ip=ip.split('/')
     p1,p2,p3,p4=map(int, ip[0].split('.'))
     if p1 <=127 and p1>=0 :
-        res=("Class: A, Designation: Public")
+        ruselt=("Class: A, Designation: Public")
         if p1==127 and p2==0 :
-            res=("Class: A, Designation: Public")
+            ruselt=("Class: A, Designation: Special")
         else:
-            res=("Class: A, Designation: Special")
+            ruselt=("Class: A, Designation: Public")
     elif p1==10:
-        res=("Class: A, Designation: Private")
+        ruselt=("Class: A, Designation: Private")
     elif p1 <=191 and p1 >=128 :
-        res=("Class: B, Designation: Public")
+        ruselt=("Class: B, Designation: Public")
         if p1==172 and p2<=31 and p2>=16:
-            res=("Class: B, Designation: Private")
+            ruselt=("Class: B, Designation: Private")
     elif p1 <=223 and p1>=192 :
-        res=("Class: C, Designation: Public")
+        ruselt=("Class: C, Designation: Private")
     elif p1==192 and p2==168:
-        res=("Class: C, Designation: Private")
+        ruselt=("Class: C, Designation: Public")
     elif p1 <=239 and p1>=224 :
-        res=("Class: D, Designation: Public")
+        ruselt=("Class: D, Designation: Public")
     elif p1 <=255 and p1>=240:
-        res=("Class: E, Designation: Public")
+        ruselt=("Class: E, Designation: Public")
  
-    print(res)
+    print(ruselt)
 
 if __name__ == '__main__':
-    pass
-    ip='192.168.1.1/24'
+    solution('127.0.0.1/24')
+    solution('192.168.1.1/24')
     
-    solution(ip)
+    pass
